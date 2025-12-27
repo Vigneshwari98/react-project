d1486787580df441770fe383d06412103d6b57a6
VenkateshSubramani <venkatstark2@gmail.com>
2025-12-27 21:07:32 +0530

    Created list group using array map

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,5 +1,15 @@
-function ListGroup(){
-    return <h1>List Group</h1>;
+function ListGroup() {
+    const items = ["Paris", "London", "India", "New York", "Switzerland"];
+
+    return (
+        <ul className="list-group">
+            {items.map((item) => (
+                <li key={item} className="list-group-item">
+                    {item}
+                </li>
+            ))}
+        </ul>
+    );
 }
 
 export default ListGroup;
