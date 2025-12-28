324be1750c6df4c3d75ee4a14a0a745b1eba2eec
VenkateshSubramani <venkatstark2@gmail.com>
2025-12-28 14:00:30 +0530

    1.Prop Interface, 2.Usestate Hook, 3.Conditionally rendered css

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import ListGroup from "./components/ListGroup";
 
-function App(){
-  return <div><ListGroup/></div>;
+function App() {
+    const cities = ["Paris", "London", "India", "New York", "Switzerland"];
+    return (
+        <div>
+            <ListGroup items={cities} heading="Cities" />
+        </div>
+    );
 }
-export default App; 
+export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,33 @@
-function ListGroup() {
-    const items = ["Paris", "London", "India", "New York", "Switzerland"];
+import { useState } from "react";
+
+interface ListGroupProps {
+    items: string[];
+    heading: string;
+}
+
+function ListGroup({ items, heading }: ListGroupProps) {
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     return (
-        <ul className="list-group">
-            {items.map((item) => (
-                <li key={item} className="list-group-item">
-                    {item}
-                </li>
-            ))}
-        </ul>
+        <>
+            <h1>{heading}</h1>
+            {items.length === 0 && <p>No item found</p>}
+            <ul className="list-group">
+                {items.map((item, index) => (
+                    <li
+                        className={
+                            selectedIndex === index
+                                ? "list-group-item active"
+                                : "list-group-item"
+                        }
+                        key={item}
+                        onClick={() => setSelectedIndex(index)}
+                    >
+                        {item}
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
 
